0d142e2d90b8777b4999c01dbf0254e00ba14b72
Szadusik <szady92@gmail.com>
2023-11-21 00:16:57 +0100

    Implemented Macaulay Matrix creation

--- implementation/macaulay_matrix.py
+++ implementation/macaulay_matrix.py
@@ -1,7 +1,8 @@
 import numpy as np
 import sympy as sp
 import itertools
-
+from polynomial_system import PolynomialSystem
+from sortedcollections import OrderedSet
 
 '''
 Calculate witness degree which used to determine degree of Macaulay matrix. Calculation based on Hilbert series. 
@@ -28,7 +29,8 @@ def calculate_witness_degree(m: int, n: int, k: int) -> int:
 
 
 '''
-Generate all monomials up to given degree. 
+Generate all monomials up to given degree. Monomials are sorted from lowest to highest degree. Additionally,
+lexicographical order is preserved between generated monomials.
 Input:
     symbols - List of symbols to be used in monomials
     d - Degree to up which monomials should be generated
@@ -38,7 +40,7 @@ Output:
 '''
 
 def generate_monomials(symbols: set, d: int) -> set:
-    monomials = set()
+    monomials = OrderedSet()
     
     if d < 0:
         raise ValueError('Degree has to be a non negative value')
@@ -56,9 +58,54 @@ def generate_monomials(symbols: set, d: int) -> set:
 
     return monomials
 
-# print(calculate_witness_degree(5,5,2))
 
-x, y, z = sp.symbols('x y z')
-symbols = set([x, y, z])
-monomials = generate_monomials(symbols, 2)
-print(monomials)
+def create_monomial_tuples(monomials: set[sp.Poly]) -> list[tuple]:
+    poly_tupples = []
+    for m in monomials:
+        mono_tuple = list(m.as_dict().keys())[0]
+        poly_tupples.append(mono_tuple)
+    return poly_tupples
+    
+
+def poly_to_vector(poly: sp.Poly, monomial_tuples: list[tuple]) -> np.ndarray:
+    poly_vector: np.ndarray = np.zeros(len(monomial_tuples))
+    terms: dict = poly.as_dict()
+    
+    for degree_tuple, coef in terms.items():
+        if degree_tuple in monomial_tuples:
+            idx = monomial_tuples.index(degree_tuple)
+            poly_vector[idx] = coef
+    
+    return poly_vector
+
+
+def create_macaulay_matrix(poly_system: PolynomialSystem, degree: int):
+    system_variables: set[sp.Symbol] = poly_system.variables
+    monomials: set[sp.Poly] = generate_monomials(system_variables, degree)
+
+    rows_polynomials = []
+    for mono in monomials:
+        d_mono = mono.degree()
+        for f in poly_system.equations:
+            d_poly = f.degree()
+            if d_mono <= degree - d_poly:
+                rows_polynomials.append(f * mono)
+    
+    column_tuples: list[tuple] = create_monomial_tuples(monomials)
+    zero_vector = np.zeros(len(column_tuples))
+
+    vectorized_p : list[np.ndarray] = []
+    for p in rows_polynomials:
+        vector = poly_to_vector(p, column_tuples)
+        if np.array_equal(vector, zero_vector):
+            continue
+        vectorized_p.append(vector)
+
+    macaulay_matrix = np.array(vectorized_p)        
+    return np.unique(macaulay_matrix, axis=0)
+
+
+'''
+Generate monomial tuples where each tuple represent a degress of each variable that makes a monomial
+
+'''
